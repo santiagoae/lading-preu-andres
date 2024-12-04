ce2da08ebb2f9bbb2aa3dbc847081ab98c69e5b1
santiagoae <santiago_avendano82162@elpoli.edu.co>
2024-12-04 13:51:58 -0500

    feature: add base colors to tailwind, finish schedule section and implement calltoactionbuttons component

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'dark-blue': '#342a61',
+        'sky-blue': '#0eb9dc'
+      }
+    },
   },
   plugins: [],
 }
